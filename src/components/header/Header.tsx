6c33707506cf34504a3df9d075b31d7797d1c899
MJTwerda <mauriciotwerda@gmail.com>
2025-02-20 20:44:02 -0300

    MD header border styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -89,13 +89,14 @@ const Header = () => {
   ];
 
   return (
-    <div className="flex justify-between m-4">
+    <div className="flex justify-between m-4 border-b-2 border-b-opacity-40">
       <div className="flex w-2/4">
         <Image
           alt='Ego Cars logo'
           src={'/ego-logo.svg'}
           width={38}
           height={40}
+          className="mr-20"
         />
         <div className="hidden sm:flex">
           <CommonTabs tabList={headerTabs} />
