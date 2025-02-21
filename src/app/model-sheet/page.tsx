da546b61eff91378699f1e9843951f0737926e96
MJTwerda <mauriciotwerda@gmail.com>
2025-02-21 18:30:23 -0300

    ADD fetch to get models on Home page

---
+++ src/app/model-sheet/page.tsx
@@ -0,0 +1,9 @@
+const ModelSheetPage = () => {
+  return (
+    <>
+      <h1>Model Sheet</h1>
+    </>
+  );
+};
+
+export default ModelSheetPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import Image from "next/image";
+import { Suspense } from "react";
+import ModelList from "@/components/models/modelList";
+import { VehicleModelI } from "@/components/models/models.interfaces";
+import { Typography } from "@mui/material";
 
-export default function Home() {
+const HomePage = async () => {
+  const getVehicleModels = async (): Promise<VehicleModelI[]> => {
+      const res = await fetch(
+        `${process.env.EGO_API}/models`, 
+        {
+          cache: "no-store",
+        }
+      );
+  
+    if (!res.ok) throw new Error("Failed to fetch models");
+    return res.json();
+  };
+
+  const modelsResponse = await getVehicleModels();
+
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="mx-4 mt-11 sm:mx-32 sm:mt-16">
+      <Typography variant="h3" gutterBottom>
+        Descubrí todos los modelos
+      </Typography>
+
+    <Suspense fallback={<p>Cargando modelos...</p>}>
+      <ModelList modelsData={modelsResponse} />
+    </Suspense>
+    </div>
   );
-}
+};
+
+export default HomePage;
